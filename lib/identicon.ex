91fdf4a5117dd36245d314315bf6af4def0dcdf6
Spencer Markin <markinspencer@gmail.com>
2019-01-03 20:59:48 -0500

    implement build pixel map, list of top left & bottom right points

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,22 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do 
+    pixel_map = Enum.map(grid, &to_pixel/1)
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def to_pixel({_code, index} = square) do 
+    horizontal = rem(index, 5) * 50
+    vertical = div(index, 5) * 50
+    
+    top_left = {horizontal, vertical}
+    bottom_right = {horizontal + 50, vertical + 50}
+    
+    {top_left, bottom_right}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
@@ -12,7 +28,7 @@ defmodule Identicon do
       rem(code, 2) == 0
     end
     %Identicon.Image{image | grid: updatedGrid}  
-  end 
+  end
 
   def build_grid(%Identicon.Image{seed: seed} = image) do 
     grid = 
