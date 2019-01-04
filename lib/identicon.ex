29df59105d05562fe6a0d9f06fb030f2e8790fcf
Spencer Markin <markinspencer@gmail.com>
2019-01-03 22:37:22 -0500

    implement generating and writing image to disc

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,14 +6,31 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
+
+  def save_image(image, filename) do 
+    File.write("#{filename}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do 
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) -> 
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end 
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do 
     pixel_map = Enum.map(grid, &to_pixel/1)
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
-  def to_pixel({_code, index} = square) do 
+  def to_pixel({_code, index}) do 
     horizontal = rem(index, 5) * 50
     vertical = div(index, 5) * 50
     
